8de899fee35aa30911c2c402c1f95e2fb11a642c
Angela Loftus <angelaloftus@yahoo.com>
2022-03-13 13:35:39 -0500

    updated images

--- src/components/pages/Resume/index.js
+++ src/components/pages/Resume/index.js
@@ -6,7 +6,14 @@ const Resume = () => (
     <p>
     https://docs.google.com/document/d/1w7XKNCPAv4n7YGbaiQek7t5rCj1CDRey9n5rUz0bquM/edit
     </p>
+    <object data="http://africau.edu/images/default/sample.pdf" type="application/pdf" width="100%" height="100%">
+      <p>Alternative text - include a link <a href="http://africau.edu/images/default/sample.pdf">to the PDF!</a></p>
+  </object>
+
+
   </div>
+
+  
 );
 
 export default Resume;
